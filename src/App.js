af1283ad990b69e9acb42f65f8150da5901c30ec
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-11-27 21:21:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
     </React.Fragment>
   );
 }
