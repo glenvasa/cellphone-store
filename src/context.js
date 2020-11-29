0870e92eaf6a6c5220f85c40fbab3fddd81c9dfc
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-11-29 11:08:49 -0500

    Changed the way products are set in State
    Instead of a reference to storeProducts we create a fresh set of products and set state

--- src/context.js
+++ src/context.js
@@ -7,25 +7,65 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct,
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+  // gets fresh set of values for each nested object(product) in storeProducts array,
+  // doesn't reference them any longer
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
 
   handleDetail = () => {
-      console.log('hello from detail');
-  }
+    console.log("hello from detail");
+  };
 
   addToCart = () => {
-      console.log('hello from add to cart')
-  }
+    console.log("hello from add to cart");
+  };
+
+  //testing to see if storeProducts matches what is in the state.products
+  //With products: storeProducts in State we are experiencing a reference issue
+  //because when we change state.products.inCart to true it also claims
+  //that storeProducts.inCart changed to true, even though the value did not actually
+  //change storeProducts in data file
+  //   tester = () => {
+  //     console.log("State products :", this.state.products[0].inCart);
+  //     console.log("Data products :", storeProducts[0].inCart);
+
+  //     const tempProducts = [...this.state.products];
+  //     tempProducts[0].inCart = true;
+
+  //     this.setState(
+  //       () => {
+  //         return { products: tempProducts };
+  //       },
+  //       () => {
+  //         console.log("State products :", this.state.products[0].inCart);
+  //         console.log("Data products :", storeProducts[0].inCart);
+  //       }
+  //     );
+  //   };
 
   render() {
     return (
-      <ProductContext.Provider value={{
+      <ProductContext.Provider
+        value={{
           ...this.state,
-          handleDetail:this.handleDetail,
-          addToCart:this.addToCart,
-      }}>
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart,
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
